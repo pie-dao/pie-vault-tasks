ff82cc775d40930c02436c40336645147ef3cc07
Mick de Graaf <mick@mickdegraaf.nl>
2021-03-01 14:30:52 +0100

    Comment out incomplete ms task

--- tasks/multisig.ts
+++ tasks/multisig.ts
@@ -1,12 +1,12 @@
 import { task } from "hardhat/config";
 import { MultisigFactory } from "../types/ethers-contracts/MultisigContract";
 
-task("submit-ms-tx")
-    .addParam("multisig", "address of the multisig")
-    // .addParam("")
-    .setAction(async(taskArgs, {ethers}) => {
-        const signers = await ethers.getSigners();
+// task("submit-ms-tx")
+//     .addParam("multisig", "address of the multisig")
+//     // .addParam("")
+//     .setAction(async(taskArgs, {ethers}) => {
+//         const signers = await ethers.getSigners();
  
-        const multisig = MultisigFactory.connect(taskArgs.multisig, signers[0]);
+//         const multisig = MultisigFactory.connect(taskArgs.multisig, signers[0]);
 
-});
+// });
